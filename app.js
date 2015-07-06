dc1743ab60a042769dd479aea731232822889955
Tom Alperin <tomalperin@Toms-MacBook-Air-5.local>
2015-07-06 14:11:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+name = prompt("What is your name?");
+email = prompt("What is your email address?");
+favMovie = prompt("What is your favorite movie?");
+var theUser = {
+	userName: name,
+	userMail: email,
+	userMovie: favMovie
+};
+console.log(theUser);
